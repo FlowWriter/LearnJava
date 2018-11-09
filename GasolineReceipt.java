0d93a13f6a4fdfcb97f5e802885e12a25d7a5cb7
FlowWriter <42384068+FlowWriter@users.noreply.github.com>
2018-11-09 15:40:11 -0500

    stuff

---
+++ GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+-----------------------------+" );
+    System.out.println( "|                             |" );
+    System.out.println( "|         CORNER STORE        |" );
+    System.out.println( "|                             |" );
+    System.out.println( "| 2018-11-08  04:38pm         |" );
+    System.out.println( "|                             |" );
+    System.out.println( "| Gallons:        10.870      |" );
+    System.out.println( "| Price/gallon:  $ 2.38       |" );
+    System.out.println( "|                             |" );
+    System.out.println( "| Fuel total:   $ 25.87       |" );
+    System.out.println( "|                             |" );
+    System.out.println( "|-----------------------------+" );
+  }
+}
